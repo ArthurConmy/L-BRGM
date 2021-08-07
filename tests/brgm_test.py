2345f765ed7ddd129953500f40070e6cdf5d0562
Arthur Conmy <arthurconmy@fb.com>
2021-08-07 14:57:40 +0100

    Committing from the shell script

--- tests/brgm_test.py
+++ tests/brgm_test.py
@@ -10,3 +10,6 @@ class TestBRGM(unittest.TestCase):
         loss_log = [entrr["total"] for entrr in brgm.loss_log]
         print("Percentage DIFF from expected BRGM losses:", percentage_diff(torch.FloatTensor(loss_log), torch.load("losslog.pt")))
         print('FIX THE ISSUE WITH LPIPS NEEDING THINGS TO BE ON SAME DEVICE')
+
+if __name__ == '__main__':
+    unittest.main()
